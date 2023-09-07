0abb458593dfc79da2de9d34d4f46ea68d358776
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-07 14:23:32 +0200

    fetched word from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+
+      useEffect(() => {
+      const fetchDrinks = async () => {
+         try {
+            const resp = await fetch(
+               "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+            );
+            const word = await resp.json();
+            console.log('word from dictionary api', word)
+
+         } catch (error) {
+            console.log('Failed to fetch data', error)
+         }
+      };
+      fetchDrinks();
+   }, []);
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+         <h1>Dictionary</h1>
     </div>
   );
 }
