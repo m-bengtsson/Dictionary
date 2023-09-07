34cb2b543a9f1410299ac766ef4a3e681749a4de
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-07 16:21:10 +0200

    error messages displaying correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import { useEffect, useState, ChangeEvent } from "react";
+import { useState, ChangeEvent } from "react";
 import "./App.css";
 
-
 function App() {
+  const [searchedWord, setSearchedWord] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-      const [searchedWord, setSearchedWord] = useState("");
-      
-      const fetchDrinks = async (searchWord : string) => {
-         try {
-            const resp = await fetch(
-               `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`
-            );
-            const word = await resp.json();
-            console.log('word from dictionary api', word)
+  const fetchDrinks = async (searchWord: string) => {
+    try {
+      const resp = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`
+      );
 
-         } catch (error) {
-            console.log('Failed to fetch data', error)
-         }
-      };
+      if (resp.status === 404) {
+        setErrorMessage(
+          "Sorry pal, we couldn't find definitions for the word you were looking for."
+        );
+        throw new Error("No Definitions Found");
+      }
 
-   const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-      console.log('input', event.currentTarget.value)
-      setSearchedWord(event.currentTarget.value)
-   }
+      const word = await resp.json();
+      console.log("word from dictionary api", word);
+    } catch (error) {
+      console.log("Failed to fetch data", error);
+    }
+  };
 
-   const onSubmit = () => {
-      fetchDrinks(searchedWord)
-   }
+  const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchedWord(event.currentTarget.value);
+  };
 
-
-
+  const onSubmit = () => {
+    if (!searchedWord) {
+      setErrorMessage("Tomt fält");
+    } else {
+      fetchDrinks(searchedWord);
+    }
+  };
 
   return (
     <div className="App">
-         <h1>Dictionary</h1>
-         <input 
-            placeholder="search for a word.."
-            onChange={inputHandler}
-            />
-         <button onClick={onSubmit}>Search</button>
-         <div><p>word information</p></div>
+      <h1>Dictionary</h1>
+      <input placeholder="search for a word.." onChange={inputHandler} />
+      <button onClick={onSubmit}>Search</button>
+      {errorMessage && <p>{errorMessage}</p>}
+      <div>
+        <p>word information</p>
+      </div>
     </div>
   );
 }
