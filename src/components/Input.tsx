e067ea1cae16a6cae310c376be53944aa539395a
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-09-10 19:15:19 +0200

    onsubmit enter key

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import { StyledInput } from "./styles/Styled";
+import { KeyboardEvent } from "react";
 
 
 interface Props {
@@ -6,14 +7,23 @@ interface Props {
   onSubmit: () => void;
 }
 
+
 function Input({ setSearchedWord, onSubmit }: Props) {
+
+const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+   if(event.key === 'Enter'){
+      onSubmit()
+      
+   }
+}
+
   return (
    <StyledInput>
-      
       <p>Search for a definition:</p>
       <input
            placeholder="search for a word.."
            onChange={(e) => setSearchedWord(e.target.value)}
+           onKeyDown={handleKeyDown}
          />
          <button onClick={onSubmit}>Search</button>
    </StyledInput>
